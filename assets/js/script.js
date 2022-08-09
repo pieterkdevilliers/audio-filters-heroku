2f6a01ae02afda99b6ca5adc56409189d71a6a8c
pieterkdevilliers <pieterkdevilliers@me.com>
2022-08-09 16:32:21 +0000

    Add calculation required if/else to determine which value should be calculated

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
 /**
  * Called by the Calculate button
  * Retreives the 6 input values required to perform the calculation
+ * If/Else statement determines value to be calculated
  */
 
 function receiveValues() {
@@ -18,12 +19,46 @@ function receiveValues() {
     let capacitanceScale = document.getElementById("capacitance-scale").value;
     let frequencyValue = document.getElementById("cut-off-frequency").value;
     let frequencyScale = document.getElementById("frequency-scale").value;
-    
+
     console.log(`${resistanceValue},
-     ${resistanceScale},
-     ${capacitanceValue},
-     ${capacitanceScale},
-     ${frequencyValue},
-     ${frequencyScale}`);
-};
+    ${resistanceScale},
+    ${capacitanceValue},
+    ${capacitanceScale},
+    ${frequencyValue},
+    ${frequencyScale}`);
+
+    if (resistanceValue === "0" && capacitanceValue !== "0" && frequencyValue !== "0") {
+        calculateResistance();
+    } else if (resistanceValue !== "0" && capacitanceValue === "0" && frequencyValue !== "0") {
+        calculateCapacitance();
+    } else if (resistanceValue !== "0" && capacitanceValue !== "0" && frequencyValue === "0") {
+        calculateFrequency();
+    } else {
+        console.log("Please complete two values")
+    };
+}
+
+/**
+ * Takes the values provided to receiveValues and calculates the resistanceValue
+ */
+
+function calculateResistance() {
+    console.log("Calculating Resistance")
+}
+
+/**
+ * Takes the values provided to receiveValues and calculates the capacitanceValue
+ */
+
+function calculateCapacitance() {
+    console.log("Calculating Capacitance")
+}
+
+/**
+ * Takes the values provided to receiveValues and calculates the frequencyValue
+ */
+
+function calculateFrequency() {
+    console.log("Calculating Frequency")
+}
 
